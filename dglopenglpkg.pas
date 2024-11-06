e0a42d49502f4ed73b204fc8f29eaf30addb3a3b
Artem V. Ageev <artem.v.ageev@gmail.com>
2024-11-06 21:15:44 +0200

    fix package

---
+++ dglopenglpkg.pas
@@ -0,0 +1,21 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit dglOpenGLpkg;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  dglOpenGL, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('dglOpenGLpkg', @Register);
+end.
